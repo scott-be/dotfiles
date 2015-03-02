7626b417cf54947162674b8ddc18235cc4f5c405
scott <sb1975@gmail.com>
2015-03-01 21:06:04 -0500

    ignore README.md files

--- make.sh
+++ make.sh
@@ -15,8 +15,13 @@ if [ "$input" -eq "1" -o "$input" -eq "2" ]; then
 	# Link files in the general folder
 	echo "Linking general folder:"
 		for file in $DIR/general/*; do
+
+			if [ ${file##*/} == "README.md" ]; then
+				continue # ignore readme files
+			fi
 			echo " [X] Linking ${file##*/}..."
 			ln -sf "$file" "$HOME/.${file##*/}"
+			
 		done
 	echo "...done"
 
@@ -24,16 +29,26 @@ if [ "$input" -eq "1" -o "$input" -eq "2" ]; then
 		# Link files in the Mac folder
 		echo "Linking Mac Folder:"
 		for file in $DIR/osx/*; do
+
+			if [ ${file##*/} == "README.md" ]; then
+				continue # ignore readme files
+			fi
 			echo " [X] Linking ${file##*/}..."
 			ln -sf "$file" "$HOME/.${file##*/}"
+
 		done
 		echo "...done"
 	else
 		# Link files in the Linux folder
 		echo "Linking Linux Folder:"
 		for file in $DIR/linux/*; do
+
+			if [ ${file##*/} == "README.md" ]; then
+				continue # ignore readme files
+			fi
 			echo " [X] Linking ${file##*/}..."
 			ln -sf "$file" "$HOME/.${file##*/}"
+
 		done
 		echo "...done"
 	fi
