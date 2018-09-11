39ae2ba68d11dde433fd9b0c396f71dc04e1a0f7
Scott <sb1975@gmail.com>
2018-09-10 22:16:20 -0400

    reorder shortcuts

--- linux/config/awesome/rc.lua
+++ linux/config/awesome/rc.lua
@@ -62,7 +62,6 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    awful.layout.suit.floating,
     awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
@@ -74,6 +73,7 @@ awful.layout.layouts = {
     -- awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier,
     awful.layout.suit.corner.nw,
+    awful.layout.suit.floating,
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
